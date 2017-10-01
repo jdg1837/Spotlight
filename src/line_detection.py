00a99398c2c6b85dc8d4bba7c99d711d7ed93c88
Chris Jakins <cjakins12@yahoo.com>
2017-10-01 02:04:10 -0500

    Move .jpg's to .png's

--- src/line_detection.py
+++ src/line_detection.py
@@ -26,6 +26,7 @@ threshold = 15  # minimum number of votes (intersections in Hough grid cell)
 min_line_length = 50  # minimum number of pixels making up a line
 max_line_gap = 150  # maximum gap in pixels between connectable line segments
 line_image = np.copy(img) * 0  # creating a blank to draw lines on
+line_overlay = np.copy(img)
 
 # Run Hough on edge detected image
 # Output "lines" is an array containing endpoints of detected line segments
@@ -38,6 +39,12 @@ for line in lines:
         cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
 
 # Draw the lines on the  image
-lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
+lines_edges = cv2.addWeighted(img, 0.8, line_overlay, 1, 0)
+
+cv2.imshow("one", img)
+cv2.imshow("two", line_image)
+cv2.imshow("three", line_overlay)
+img += line_image
+cv2.imshow("four", img)
 
 cv2.imwrite(imgOutput, line_image)
